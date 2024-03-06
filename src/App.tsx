60e9df935cb944b7814202e9a31a94490e52adde
hayuuna <luxureny@gmail.com>
2024-03-06 20:13:18 +0900

    Setting: Project inital setting

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+import '@/App.css';
+
+import Home from '@src/pages/Home';
+
+function App() {
+	// eslint-disable-next-line react/react-in-jsx-scope
+
+	return (
+		<div>
+			<Home />
+		</div>
+	);
+}
+
+export default App;
